4f249d004e59816abab2e414a8828c641dbb6a4e
Dhiman <dhimannayak003@gmail.com>
2023-12-29 14:36:22 +0530

    MongoDB connected

---
+++ src/index.js
@@ -0,0 +1,39 @@
+import dotenv from "dotenv";
+dotenv.config({path:"./env"})
+import express  from "express";
+import mongoose from "mongoose";
+import { DB_NAME } from "./constants.js";
+const app = express()
+import connectDB from "./db/index.js";
+
+connectDB()
+.then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`⚙️ Server is running at port : ${process.env.PORT}`);
+    })
+})
+.catch((err) => {
+    console.log("MONGO db connection failed !!! ", err);
+})
+
+
+
+
+
+/*
+require("dotenv").config({path:"./env"})
+(async ()=>{
+    try {
+        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+        app.on("error",(error)=>{
+            console.log("Errorr" , error);
+            throw error;
+        });
+        app.listen(`${process.env.PORT}`,()=>{
+            console.log(`${process.env.PORT}`)
+        })
+    } catch (error) {
+        console.log("Error", error)
+        throw error;
+    }
+})()*/
