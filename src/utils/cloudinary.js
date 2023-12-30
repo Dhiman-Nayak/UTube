13dee1b3a28c18524525aeab05b745cee6687819
Dhiman <dhimannayak003@gmail.com>
2023-12-30 13:33:38 +0530

    connect cloudinary

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,27 @@
+import { v2 as cloudinary} from "cloudinary";
+import fs from "fs"
+
+          
+cloudinary.config({ 
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+  api_key: process.env.CLOUDINARY_API_KEY, 
+  api_secret: process.env.CLOUDINARY_API_SECRET
+});
+
+const uploadCloudinary= async (localFilePath)=>{
+    try {
+        if(!localFilePath) return null
+        //upload file to cloudinary
+        const response = await cloudinary.v2.uploader.upload(localFilePath,{
+            resource_type:"auto"
+        })
+        //file upload successfully
+        console.log("File uploaded :", response.url);
+        return response;
+    } catch (error) {
+        fs.unlinkSync(localFilePath) //remove the locally saved temporary file
+        return null
+    }
+}
+
+export {uploadCloudinary}
