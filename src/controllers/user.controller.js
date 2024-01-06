6eea0852b2bd3df30a8094ceb923c753d1d1fc0f
Dhiman <dhimannayak003@gmail.com>
2024-01-06 19:45:29 +0530

    add change Password and getUser endpoint

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -217,4 +217,38 @@ const refreshAccessTokene=asyncHandler(async(req,res)=>{
     }
 })
 
-export { registerUser, login, logout,refreshAccessTokene };
+const changeCurrentPassword=asyncHandler(async(req,res)=>{
+    const {oldPassword,newPassword}=req.body;
+    if(oldPassword === newPassword) throw new ApiError(400,"Both are same password")
+    const user= await User.findById(req.user?.id)
+    const isPasswordCorrect=await user.isPasswordCorrect(oldPassword)
+    if(!isPasswordCorrect){
+        throw new ApiError(400,"Invalid old password")
+    }
+    user.password=newPassword
+    await user.save({validateBeforeSave:false})
+
+    return res
+            .status(200)
+            .json(new ApiResponse(200, {}, "Password changed successfully"))
+})
+
+const getCurrentUser = asyncHandler(async(req, res) => {
+    return res
+    .status(200)
+    .json(new ApiResponse(
+        200,
+        req.user,
+        "User fetched successfully"
+    ))
+})
+
+export { 
+    registerUser, 
+    login, 
+    logout,
+    refreshAccessTokene ,
+    changeCurrentPassword,
+    getCurrentUser,
+    
+};
