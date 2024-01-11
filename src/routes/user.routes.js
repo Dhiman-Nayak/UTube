44c5d6035ea8198be7006b37f48ab646612a2273
Dhiman <dhimannayak003@gmail.com>
2024-01-11 20:36:06 +0530

    add some user routes

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -29,13 +29,19 @@ router.route("/register").post(
     registerUser
 );
 
-router.route("/login").post(login);
-router.route("/logout").post(verifyJWT, logout);
-router.route("/refresh-token").post(refreshAccessTokene);
-router.route("/change-password").post(verifyJWT,changeCurrentPassword);
-router.route("/users").post(verifyJWT,getCurrentUser);
-router.route("/update-account").post(verifyJWT,updateAccountDetails);
-router.route("/update-avatar").post(verifyJWT,updateUserAvatar);
-router.route("/update-coverimage").post(verifyJWT,updateUserCoverImage);
+router.route("/login").post(loginUser)
+
+//secured routes
+router.route("/logout").post(verifyJWT,  logoutUser)
+router.route("/refresh-token").post(refreshAccessToken)
+router.route("/change-password").post(verifyJWT, changeCurrentPassword)
+router.route("/current-user").get(verifyJWT, getCurrentUser)
+router.route("/update-account").patch(verifyJWT, updateAccountDetails)
+
+router.route("/avatar").patch(verifyJWT, upload.single("avatar"), updateUserAvatar)
+router.route("/cover-image").patch(verifyJWT, upload.single("coverImage"), updateUserCoverImage)
+
+router.route("/c/:userName").get(verifyJWT, getUserChannelProfile)
+router.route("/history").get(verifyJWT, getWatchHistory)
 
 export default router;
